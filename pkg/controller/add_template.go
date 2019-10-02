c1915c133234780d7886b0ed25e6204897c79222
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2019-10-02 10:22:25 +0300

    [EPMDEDP-3835] Disable registering Template controller according to env variable PLATFORM_TYPE
    Change-Id: I3cec2ebdd5278e8c0c7cdd79cdc684312319e60f

--- pkg/controller/add_template.go
+++ pkg/controller/add_template.go
@@ -2,9 +2,12 @@ package controller
 
 import (
 	"github.com/epmd-edp/reconciler/v2/pkg/controller/template"
+	"github.com/epmd-edp/reconciler/v2/pkg/service/platform"
 )
 
 func init() {
-	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, template.Add)
+	if platform.IsOpenshift() {
+		// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+		AddToManagerFuncs = append(AddToManagerFuncs, template.Add)
+	}
 }
